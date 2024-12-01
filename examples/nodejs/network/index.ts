9ac9e4c4358463521fd645883d1dbdec334d89dd
rawdaGastan <rody.rawda.rr@gmail.com>
2024-12-01 13:14:54 +0200

    fix nodejs network example

--- examples/nodejs/network/index.ts
+++ examples/nodejs/network/index.ts
@@ -3,7 +3,7 @@ import * as threefold from "@threefold/pulumi";
 var rand_str = Math.random().toString(36).slice(8)
 
 const provider = new threefold.Provider("provider", {mnemonic: process.env.MNEMONIC, network: process.env.NETWORK});
-const scheduler = new threefold.Scheduler("scheduler", {farm_ids: [1]}, {
+const scheduler = new threefold.Scheduler("scheduler", {farm_ids: [1], ygg: true}, {
     provider: provider,
 });
 const network = new threefold.Network("network", {
@@ -12,7 +12,6 @@ const network = new threefold.Network("network", {
     nodes: [scheduler.nodes[0]],
     ip_range: "10.1.0.0/16",
 		mycelium: true,
-		ygg: true,
 }, {
     provider: provider,
     dependsOn: [scheduler],
